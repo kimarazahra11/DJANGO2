73c117d1796c172e07868117bb66d377a8b764be
zahrahabibimini <139326429+zahrahabibimini@users.noreply.github.com>
2023-08-22 17:11:31 +0430

    create template

--- root/admin.py
+++ root/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Services
 
 
-# Register your models here.
+
+class adminservices(admin.ModelAdmin):
+    list_display = ['title','content','status']
+    list_editable = ["status"]
+    search_fields = ['title']
+   
+admin.site.register(Services,adminservices)   
+
+
